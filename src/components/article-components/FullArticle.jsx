bab99e7621033962a3de912ef9885d541de7cfc4
Little-Wolfie <kiefkostello@gmail.com>
2023-04-19 10:24:39 +0100

    Implemented upvote button to vote on articles

--- src/components/article-components/FullArticle.jsx
+++ src/components/article-components/FullArticle.jsx
@@ -1,4 +1,46 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../api';
+
 const FullArticle = ({ article }) => {
+	const [hasVoted, setHasVoted] = useState(false);
+	const [votes, setVotes] = useState(article.votes);
+
+	const handleUpvoteButton = e => {
+		api
+			.upvoteArticle(article.article_id, e.target.value)
+			.then(() => {
+				setVotes(current => {
+					return Number(current) + Number(e.target.value);
+				});
+
+				if (e.target.value == 1) {
+					setHasVoted(true);
+				} else {
+					setHasVoted(false);
+				}
+			})
+			.catch(err => {
+				alert('Could not upvote at this time, try again later.');
+			});
+	};
+
+	useEffect(() => {
+		const hasVotedFromStorage = localStorage.getItem(
+			`hasVoted-${article.article_id}`
+		);
+
+		if (hasVotedFromStorage) {
+			setHasVoted(JSON.parse(hasVotedFromStorage));
+		}
+	}, [article.article_id]);
+
+	useEffect(() => {
+		localStorage.setItem(
+			`hasVoted-${article.article_id}`,
+			JSON.stringify(hasVoted)
+		);
+	}, [article.article_id, hasVoted]);
+
 	return (
 		<article className='full-article-container'>
 			<section className='article-content'>
@@ -7,6 +49,7 @@ const FullArticle = ({ article }) => {
 				<div className='divider-small'></div>
 				<div className='author-date-upvote-container'>
 					<h4>Author: {article.author}</h4>
+
 					<h5>
 						Created:{' '}
 						{new Date(article.created_at)
@@ -15,11 +58,27 @@ const FullArticle = ({ article }) => {
 							.slice(1, 4)
 							.join(' ')}
 					</h5>
+
 					<div className='upvote-btn'>
-						<h5>votes: {article.votes}</h5>
-						<button>Upvote</button>
+						<h5>votes: {votes}</h5>
+						{hasVoted ? (
+							<button
+								value={-1}
+								onClick={handleUpvoteButton}
+							>
+								Upvoted!
+							</button>
+						) : (
+							<button
+								value={1}
+								onClick={handleUpvoteButton}
+							>
+								Vote
+							</button>
+						)}
 					</div>
 				</div>
+
 				<div className='divider-small'></div>
 
 				<img
@@ -30,7 +89,6 @@ const FullArticle = ({ article }) => {
 				<p>{article.body}</p>
 			</section>
 			<div className='divider-small'></div>
-			<section className='comment-content'></section>
 		</article>
 	);
 };
