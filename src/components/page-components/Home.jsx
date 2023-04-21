a158af0368018759b7f1e7dfb6244ff1b3277833
Little-Wolfie <kiefkostello@gmail.com>
2023-04-21 12:52:37 +0100

    Include search params in url when first loading instead of loading to just /articles

--- src/components/page-components/Home.jsx
+++ src/components/page-components/Home.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import ArticleContainer from '../home-components/ArticleContainer';
 import Sort from '../home-components/Sort';
 import { api } from '../../api';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Home = () => {
+	const navigate = useNavigate();
 	const [articles, setArticles] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [searchParams, setSearchParams] = useSearchParams();
@@ -13,6 +14,7 @@ const Home = () => {
 		const sortParam = searchParams.get('sort_by') || 'created_at';
 		const orderParam = searchParams.get('order_by') || 'desc';
 		const sortString = `?sort_by=${sortParam}&order_by=${orderParam}`;
+		navigate(`/articles${sortString}`);
 
 		api.fetchArticles(sortString).then(res => {
 			setArticles(res);
